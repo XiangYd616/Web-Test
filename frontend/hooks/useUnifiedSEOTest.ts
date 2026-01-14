bc343cc4d9fac832dfb20464b4afb80c78ab3232
XiangYd616 <xyd91964208@outlook.com>
2026-01-14 13:52:19 +0800

    refactor: 删除9个重复的unified文件，已有对应的非unified版本

--- frontend/hooks/useUnifiedSEOTest.ts
+++
@@ -1,9 +0,0 @@
-﻿// Stub file - Unified SEO Test Hook
-import { useState } from 'react';
-
-export const useUnifiedSEOTest = () => {
-  const [result, setResult] = useState<any>(null);
-  return { result, setResult };
-};
-
-export default useUnifiedSEOTest;
